1fd6060f99d068f24ad9d9b7346b62e71c3432ed
Matt Malone <mattagape@me.com>
2024-12-30 17:01:35 -0600

    updates

---
+++ functions/helpers/email-content.js
@@ -0,0 +1,12 @@
+// functions/helpers/email-content.js
+
+import { 
+    body 
+} from './body.js';
+
+const emailContent = {
+    subject: "Welcome to the Man of Destiny Newsletter!",
+    body: body
+};
+
+export { emailContent };
--- functions/helpers/sendEmailWithMailgun.js
+++ functions/helpers/sendEmailWithMailgun.js
@@ -1,28 +1,25 @@
+// functions/helpers/sendEmailWithMailgun.js
+
 import formData from 'form-data';
 import Mailgun from 'mailgun.js';
+import { emailContent } from './email-content.js'; // Import email subject and body
 
 // Initialize Mailgun with form data
 const mailgun = new Mailgun(formData);
 const mg = mailgun.client({
-  username: 'api',
-  key: process.env.MAILGUN_API_KEY || 'key-yourkeyhere', // Replace 'key-yourkeyhere' with your test Mailgun key for now
+    username: 'api',
+    key: process.env.MAILGUN_API_KEY || 'key-yourkeyhere', // Replace 'key-yourkeyhere' with your test Mailgun key for now
 });
 
 // Function to send an email
 const sendEmailWithMailgun = async (email) => {
-    const subject = "Welcome to the Newsletter!";
-    const body = `
-        <h1>Welcome!</h1>
-        <p>Thank you for signing up for the Man of Destiny Newsletter. Stay tuned for amazing updates!</p>
-    `;
-
     try {
-        await mg.messages.create(process.env.MAILGUN_DOMAIN || 'yourdomain.mailgun.org', {
-            from: `Matt Malone - Author <info@${process.env.MAILGUN_DOMAIN || 'yourdomain.mailgun.org'}>`,
+        await mg.messages.create(process.env.MAILGUN_DOMAIN || 'soaw4life@gmail.com', {
+            from: `Matt Malone - Author <info@${process.env.MAILGUN_DOMAIN || 'soaw4life@gmail.com'}>`,
             to: [email],
-            subject,
+            subject: emailContent.subject, // Use subject from emailContent
             text: "Thank you for signing up for the newsletter!", // Plain text fallback
-            html: body, // Styled HTML email
+            html: emailContent.body, // Use HTML body from emailContent
         });
         console.log("Email sent successfully");
     } catch (error) {
