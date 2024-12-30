432b76c53315a8cc8994d3cd45b26e41c5251192
Matt Malone <mattagape@me.com>
2024-12-30 16:13:59 -0600

    updates

--- functions/utils/buildResponse.js
+++ functions/utils/buildResponse.js
@@ -1,19 +1,22 @@
-// functions/utils/buildResponse.js
+const allowedOrigins = [
+    'https://www.sourceofallwealth.com',
+    'https://sourceofallwealth.com',
+];
 
-// Define common headers for CORS
-const commonHeaders = {
-    'Access-Control-Allow-Origin': 'https://sourceofallwealth.com, https://www.sourceofallwealth.com',
-    'Access-Control-Allow-Headers': 'Content-Type',
-    'Access-Control-Allow-Methods': 'POST, OPTIONS',
-    'Access-Control-Allow-Credentials': true,
+const buildResponse = (statusCode, body, event) => {
+    const origin = event.headers.origin;
+    const allowOrigin = allowedOrigins.includes(origin) ? origin : 'https://www.sourceofallwealth.com';
+
+    return {
+        statusCode,
+        headers: {
+            'Access-Control-Allow-Origin': allowOrigin,
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Methods': 'POST, OPTIONS',
+            'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify(body),
+    };
 };
 
-// Helper function for building the response
-const buildResponse = (statusCode, body) => ({
-    statusCode,
-    headers: commonHeaders,
-    body: JSON.stringify(body),
-});
-
-// Export the buildResponse function
 export default buildResponse;
