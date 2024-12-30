fbe0d9469a8582bf69e8cdaa00de810b6cf274fd
Matt Malone <mattagape@me.com>
2024-12-30 17:16:33 -0600

    updates

--- functions/helpers/body.js
+++ functions/helpers/body.js
@@ -1,12 +1,15 @@
-export const body = 
-    `
-        <p class="paragraph">
-            I wanted to let you know that your book is on the docket and will head toward the <b>PRINTING PRESSES</b> soon!.
-        </p>
-        <p class="paragraph">
-            <b>You can look forward to a top quality "print-on-demand" edition of your new book coming your way!</b>
-        </p>
-        <p class="paragraph">
-            If you have any questions, please write back to me, Matt Malone, at this email address, <a href="mailto:info@sourceofallwealth.com">info@sourceofallwealth.com</a>!
-        </p>
-    `;
+import fs from 'fs';
+import path from 'path';
+import MarkdownIt from 'markdown-it';
+
+// Initialize Markdown-it
+const md = new MarkdownIt();
+
+// Read the Markdown file
+const markdownPath = path.resolve(__dirname, '../markdown/newsletter-1.md');
+const markdownContent = fs.readFileSync(markdownPath, 'utf8');
+
+// Convert Markdown to HTML
+const body = md.render(markdownContent);
+
+export { body };
