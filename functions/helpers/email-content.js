83a3767d536b0101d0d811919e0eafaf0149897b
Matt Malone <mattagape@me.com>
2025-01-09 14:59:22 -0600

    updates

--- functions/helpers/email-content.js
+++ functions/helpers/email-content.js
@@ -3,7 +3,7 @@
 import { body } from './body.js';
 
 const emailContent = {
-    subject: "Man of Destiny Newsletter! 1/8/25",
+    subject: "Man of Destiny Newsletter! 1/9/25",
     body: `${body}
     <p style="font-size: 14px; color: #777; text-align: center; margin-top: 20px;">
         If you no longer wish to receive these emails, you can 
