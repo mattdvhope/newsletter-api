a9a750cd9b17a1d46c66c17c98033efda8d0b4c0
Matt Malone <mattagape@me.com>
2025-01-17 15:45:08 -0600

    updates

--- functions/helpers/body.js
+++ functions/helpers/body.js
@@ -19,29 +19,29 @@ const body = `
 
             <ul style="padding-left: 20px;">
                 <li>Three hard stats proving why dating apps are worse than useless for about 90% of men...</li>
-                <li>The glorious truth of what ultimately attracts a woman to you. <b>Hint:</b> It’s not so much about you at all.</li>
+                <li>The truth of what ultimately attracts a woman to you. <b>Hint:</b> It’s not so much about you at all.</li>
                 <li>The “silver ticket” that trumps all else in drawing a top-of-the-line young woman to you...</li>
             </ul>
 
-            <p>As you probably know already, vast numbers of single men these days are in a state of crisis. They simply cannot find a wife. Dating apps have destroyed the playing field for a huge majority of them. Let me show you the grim statistics:</p>
+            <p>As you probably know already, vast numbers of single men these days live in a state of crisis. They simply cannot find a wife. Dating apps have destroyed the playing field for a huge majority of them. Let me show you the grim statistics:</p>
 
             <ol style="padding-left: 20px;">
-                <li> It’s a man’s game of musical chairs—with no chairs left for most.<br>On apps like Tinder, men outnumber women 3 to 1. That means for every woman, there are three men swiping, competing, and losing—hard. 80-90% of men are fighting over scraps.</li>
-                <li>Women swipe right on just 10% of men—the rest don’t exist.<br>The top 10% get all the attention. If you’re not in that elite club, you’re invisible—just another name in a sea of rejected profiles.</li>
-                <li>Fake profiles and scammers are your most consistent matches.<br>More than 60% of men under fifty report encountering scammers or fake profiles. Your “dream girl” is probably a bot—or worse, a “Nigerian Prince” after your wallet.</li>
+                <li> It’s a man’s game of musical chairs -- with no chairs left for most.<br>On apps like Tinder, men outnumber women 3 to 1. That means for every woman, there are three men swiping, competing, and losing -- hard.<br>80-90% of men are fighting over scraps.</li>
+                <li>Women swipe right on just 10% of men -- the rest don’t exist.<br>The top 10% get all the attention. If you’re not in that elite club, you’re invisible -- just another name in a sea of rejected profiles.</li>
+                <li>Fake profiles and scammers are your most consistent matches.<br>More than 60% of men under fifty report encountering scammers or fake profiles.<br>Your “dream girl” is likely a bot -- or worse, a “Nigerian Prince” after your wallet.</li>
             </ol>
 
             <p>Dating apps truly are “worse than useless” because they drain away great swaths of your time while frustrating and destroying your self esteem. They steal the energy and creativity you could have put into answering this question:</p>
 
-            <p>What magnetizes a woman to a man?</p>
+            <p>What will magnetize a woman to me?</p>
 
-            <p>What ultimately attracts a woman to a man is not his looks, status, or wealth—but his ability to make her feel safe, seen, and valued on a deeply emotional level. In all likelihood, you’ve heard probably something like this before: “It is how you make her <i>feel</i> that’s important.” Nothing new here, right? But here’s the twist...</p>
+            <p>What ultimately attracts a woman to a man is not his looks, status, or wealth -- but his ability to make her feel safe, seen, and valued on a deeply emotional level.<br>In all likelihood, you’ve heard probably something like this before: “It is how you make her <i>feel</i> that’s important.”<br>Nothing new here, right? But here’s the twist...</p>
 
-            <p>While making her “feel” positive vibes towards you is crucial, you’re still left with a huge question: <i>HOW do I make her “feel” this way?</i> What if, while you may have a heart of gold, you happen to look like Elmer Fudd? What will stir up ‘that special feeling’ for you in the heart of a top-of-the-line young woman? 👩  Wait until you hear this...</p>
+            <p>While making her “feel” positive vibes towards you is crucial, you’re still left with a huge question: <i>HOW do I make her “feel” this way?</i><br>What if, while you may have a heart of gold, you happen to look like Elmer Fudd?<br>What will stir up ‘that special feeling’ for you in the heart of a top-of-the-line young woman? 👩<br>Wait until you hear this...</p>
 
-            <p>What if the key to attracting her isn’t so much about you, but about who you know?</p>
+            <p>What if the key to attracting a Proverbs 31 woman isn’t so much about you, but about who you know?</p>
 
-            <p>Here’s the truth most men miss: a woman’s attraction should not hinge on sharp features or an impressive résumé. You want her attraction to be sparked by the unspoken—and spoken—endorsements of people she respects. When the trusted men in her life—her father, brothers, mentors, or colleagues—speak highly of you, you gain an edge no swipe on a screen can deliver. Women (far more than men) are wired to notice this social proof. It’s unconscious. And it’s powerful. When respected men vouch for you, it’s like a “silver ticket” to her trust and admiration. This isn’t about “charm” or “luck.” It’s about understanding this hidden rule of attraction that most men don’t realize exists. And once you unlock it, everything changes.</p>
+            <p>Here’s the truth most men miss:<br>a woman’s attraction should not hinge on sharp features or an impressive résumé. You want her attraction to be sparked by the unspoken—and spoken—endorsements of people she respects. When the trusted men in her life—her father, brothers, mentors, or colleagues—speak highly of you, you gain an edge no swipe on a screen can ever deliver. Women (far more than men) are wired to notice this “social proof.” It’s unconscious. And it’s powerful. When respected men vouch for you, it’s like a “silver ticket” to her trust and admiration. This isn’t about “charm” or “luck.” It’s about understanding this hidden rule of attraction that most men don’t realize exists. And once you unlock it, everything changes.</p>
 
 
         </div>
@@ -51,8 +51,14 @@ const body = `
                 Do you, {{first_name_or_nickname}}, wish to obtain this “silver ticket?”
             </p>
             <p style="font-style: italic; font-weight: bold;">
-                My book, “<a href="https://www.sourceofallwealth.com/get-p-thirty-one/?first_name_or_nickname={{first_name_or_nickname}}&email={{email}}" target="_blank" style="text-decoration: underline; font-style: italic; font-weight: bold; color: inherit;">How to Find the Ideal Wife: A Proverbs 31 Woman!</a>"
+                My book, “<a href="https://www.sourceofallwealth.com/get-p-thirty-one/?first_name_or_nickname={{first_name_or_nickname}}&email={{email}}" style="text-decoration: underline; font-style: italic; font-weight: bold; color: inherit;">How to Find the Ideal Wife: A Proverbs 31 Woman!</a>"
                  shows you how.
+            </p>
+        </div>
+
+        <div style="font-family: 'Courier Prime', monospace; font-size: 32px; line-height: 1.3; text-align: left; color: #9b031b; margin-top: 40px;">
+            <p style="font-style: italic; font-weight: bold;">
+                You will learn...
             </p>
             <ul style="font-style: italic; font-weight: bold;">
                 <li>How to gain a completely unfair advantage over 99.9% of your competition--making them irrelevant (page 26).</li>
@@ -60,10 +66,6 @@ const body = `
                 <li>The secret to combining your “boyhood dreams” with your “wife of the ages” -- to conquer your all-important “net worth” issue. (page 62)</li>
                 <li>...and so much more!</li>
             </ul>                
-        </div>
-
-        <div style="border: 2px solid #ccc; border-radius: 8px; padding: 10px; margin: 20px 0; background-color: #f9f9f9;">
-            <img src="https://drive.google.com/uc?id=1QPedCfBJBRd2Zng2KpsNb0Wk4NM5FsyS" alt="Proverbs 31 Woman" style="width: 100%; border-radius: 8px;">
         </div>
 
         <div style="font-family: 'Courier Prime', monospace; font-size: 42px; line-height: 1.5; text-align: center; color: #266e19; margin-top: 40px;">
